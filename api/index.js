482e7b63383359e24badf76d59bdf6cee065c1bc
ibrahimghellab <ghellabibrahim@gmail.com>
2025-05-24 01:50:15 +0200

    style: format code for consistency and readability

--- api/index.js
+++ api/index.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const { PrismaClient } = require('@prisma/client')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+require('dotenv').config();
 
-const app = express()
-const prisma = new PrismaClient()
+const app = express();
+const prisma = new PrismaClient();
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 // Importer les routes
-const imprimerieRoute = require('../routes/controllerResultImprimerie')
-const routeDeLaSoieRoute = require('../routes/controllerResultRouteDeLaSoie')
-const cercleDesSavoirsRoute= require('../routes/controllerResultRouteDeLaSoie')
+const imprimerieRoute = require('../routes/controllerResultImprimerie');
+const routeDeLaSoieRoute = require('../routes/controllerResultRouteDeLaSoie');
+const cercleDesSavoirsRoute = require('../routes/controllerResultRouteDeLaSoie');
 
-app.use('/imprimerie', imprimerieRoute)
-app.use('/routeDeLaSoie', routeDeLaSoieRoute)
-app.use('/cercleDuSavoir', cercleDesSavoirsRoute)
+app.use('/imprimerie', imprimerieRoute);
+app.use('/routeDeLaSoie', routeDeLaSoieRoute);
+app.use('/cercleDuSavoir', cercleDesSavoirsRoute);
 
-
-
-
+// Route de test
 app.get('/', (req, res) => {
-  res.send('API host sur Vercel API opérationnelle 🚀')
-})
+  res.send('API host sur Vercel API opérationnelle 🚀');
+});
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`)
-})
+// Export Express app en tant que Serverless handler pour Vercel
+const server = require('http').createServer(app);
+module.exports = (req, res) => {
+  server.emit('request', req, res);
+};
