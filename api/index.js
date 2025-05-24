d1d8b4399e798d788124851921d27f05a15cad80
ibrahimghellab <ghellabibrahim@gmail.com>
2025-05-24 02:02:48 +0200

    fix: correct route import for cercleDesSavoirs in index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Importer les routes
 const imprimerieRoute = require('../routes/controllerResultImprimerie');
 const routeDeLaSoieRoute = require('../routes/controllerResultRouteDeLaSoie');
-const cercleDesSavoirsRoute = require('../routes/controllerResultRouteDeLaSoie');
+const cercleDesSavoirsRoute = require('../routes/controllerResultCercleDesSavoirs'); // <-- doit être un autre fichier
 
 app.use('/imprimerie', imprimerieRoute);
 app.use('/routeDeLaSoie', routeDeLaSoieRoute);
